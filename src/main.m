319f0534bae3a18be0870fc6e031277812d12262
mcd5703 <mcd5703@psu.edu>
2026-09-02 02:43:21 -0400

    Achieved proper temporal and spatial arrays

--- src/main.m
+++ src/main.m
@@ -6,7 +6,7 @@ clear; close all; clc;
 
 %% GIVEN PARAMETERS
 
-% GEOMETRY GIVENS
+% --- GEOMETRY GIVENS ---
 
 % x bounds
 xmin = 0; 
@@ -20,14 +20,14 @@ ymax = 2;
 tmin = 0;
 tmax = 1.0; % (this is also T)
 
-% time step (chosen, not given)
-dt = 0.001;
+% CFL < 1 (to calculate dt using a convective restriction only)
+CFL = 0.01;
 
 % node counts
 Nx = 81;
 Ny = 81;
 
-% PHYSICS GIVENS
+% --- PHYSICS GIVENS ---
 
 % convection velocities
 ux = 0.8;
@@ -36,20 +36,45 @@ uy = 0.8;
 % viscosity
 v = 0.01; % nu
 
-%% SIMPLE CALCULATIONS
+% --- ACTUAL SOLUTION ---
+
+% function handle
+phi_True = @(t,x,y) (1/(4*t+1)) ...
+                    * exp(-((x-ux*t-0.5)^2+(y-uy*t-0.5)^2)/(v*(4*t+1)));
+
+
+%% SPATIAL AND TEMPORAL ARRAYS
 
 % cell counts (intervals calculated from node counts)
 xCells = Nx-1;
 yCells = Ny-1;
 
-% geometric steps
+% spatial steps
 dx = (xmax - xmin) / xCells; 
 dy = (ymax - ymin) / yCells;
 
-% geometry arrays
+% temporal step calculation using convective CFL restriction
+dtx = dx * CFL / abs(ux);
+dty = dy * CFL / abs(uy);
+dt = min(dtx,dty);
+
+% time steps is found by reversing the process for space
+Nt = ceil((tmax - tmin) / dt) + 1; % (+1 converts intervals to nodes)
+% ceil is used to ensure that in the worst case (when the range isn't 
+% divisible well by dt), there are more time-steps than necessary
+
+% spatial arrays
 x = linspace(xmin, xmax, Nx);
 y = linspace(ymin, ymax, Ny);
 
-% grid
-[X,Y] = meshgrid(x,y);
+% temporal array
+t = linspace(tmin, tmax, Nt);
+
+
+%% SOLVER
+
+% allocate 2D phi array
+
+
+
 
