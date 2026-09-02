36976b9a127e7502cd95c4398456565f6fc9a4f2
mcd5703 <mcd5703@psu.edu>
2026-09-01 23:01:25 -0400

    added matlab backup files to .gitignore added input parameters to the code

--- src/main.m
+++ src/main.m
@@ -4,4 +4,36 @@
 
 clear; close all; clc;
 
-% Given parameters
+
+%% GIVEN PARAMETERS
+
+% GEOMETRY GIVENS
+
+% x bounds
+xmin = 0; 
+xmax = 2;
+
+% y bounds
+ymin = 0;
+ymax = 2;
+
+% time bounds
+tmin = 0;
+tmax = 1.0; % (this is also T)
+
+% node counts
+Nx = 81;
+Ny = 81;
+
+% PHYSICS GIVENS
+
+% convection velocities
+ux = 0.8;
+uy = 0.8;
+
+% viscosity
+v = 0.01;
+
+%% 
+
+
